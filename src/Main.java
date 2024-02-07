6f2f9328918e5d3473376bca5e843e27dbbad15d
Abdulaziz Altwaijri <aziz.f.twj@gmail.com>
2024-02-07 22:54:41 +0300

    Second, spelling

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+        spill("Twaijri");
+    }
+
+    public static void spill(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            char letter = name.charAt(i);
+            System.out.println("i = " + letter);
+        }
     }
 }
