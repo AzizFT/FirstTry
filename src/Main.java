8a13d6a950e8368027c0e542f6a23e6bd62cf028
Abdulaziz Altwaijri <aziz.f.twj@gmail.com>
2024-02-07 23:10:22 +0300

    Third, even or odd?

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello and welcome!");
+        System.out.println("Abdulaziz Altwaijri");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
 
         spill("Twaijri");
+        System.out.println("Even or odd? " + meathod(100));
     }
 
     public static void spill(String name) {
@@ -16,5 +18,11 @@ public class Main {
             char letter = name.charAt(i);
             System.out.println("i = " + letter);
         }
+    }
+
+    public static String meathod(int num) {
+        if (num % 2 == 0)
+            return "Even";
+        else return "Odd";
     }
 }
